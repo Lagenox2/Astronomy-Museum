8444738f9c1b2f3a0d568378cc694833f0baf5c7
Lagenox2 <lagenox2@gmail.com>
2025-05-16 21:38:53 +0300

    commit

--- mineshift.cmd
+++ mineshift.cmd
@@ -4,5 +4,4 @@ echo Compilation...
 C:\dev\jdk\jdk-21.0.1\bin\javac -classpath .\lwjgl\* -d .\out .\src\com\company\Main.java
 
 echo Running...
-rem _launcher.cmd
-_launcher_test.cmd
+_launcher.cmd
